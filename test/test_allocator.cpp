7023e65613ad36718bf9868cd200fd3f54dacfaf
akrozhin <akrozhin@miem.hse.ru>
2023-04-20 18:17:19 +0300

    Impl test for allocator

---
+++ test/test_allocator.cpp
@@ -0,0 +1,25 @@
+#include "../src/mmpack.hpp"
+#include <gtest/gtest.h>
+using namespace mmpack;
+
+TEST(allocator, vanilla) {
+    aligned_allocator<mm_scalar, 64> alloc;
+
+    float* p = alloc.allocate(300);
+    alloc.deallocate(p, 300);
+}
+
+TEST(allocator, vector) {
+    std::vector<mm_scalar, aligned_allocator<mm_scalar, 64>> vec_t;
+
+    for (size_t i = 0; i < 1000; i++) {
+        vec_t.push_back((mm_scalar)i);
+    }
+}
+
+TEST(allocator, allocate) {
+    aligned_allocator<float, 64> alloc;
+    float* ptr = nullptr;
+    ptr = alloc.allocate(100 * sizeof(float));
+    ASSERT_TRUE(ptr != nullptr);
+}
