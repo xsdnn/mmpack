230b5f73d1fef6e83598660d39a4e491de8c53e6
akrozhin <akrozhin@miem.hse.ru>
2023-04-20 17:47:29 +0300

    Add gtest in cmake conf

--- cmake/mmpack_unittest.cmake
+++ cmake/mmpack_unittest.cmake
@@ -1,3 +1,24 @@
+set (GTEST_ROOT ${MMPACK_ROOT}/cmake/external/googletest)
+find_package(GTest REQUIRED)
+if(NOT GTest_FOUND)
+    message(FATAL_ERROR "Not found Gtest")
+endif()
+
+
 set(MMPACK_TEST_ROOT ${MMPACK_ROOT}/test)
 
-message("IM HERE")
+macro(AddTest TARGET SOURCES LIBS)
+    add_executable(${TARGET} ${SOURCES})
+    target_link_libraries(${TARGET} ${LIBS} GTest::gtest_main)
+    add_test(${TARGET} ${TARGET})
+endmacro()
+
+set(mmpack_common_src 
+    ${MMPACK_TEST_ROOT}/test_allocator.cpp
+    )
+
+AddTest(
+    mmpack_common_test
+    ${mmpack_common_src}
+    mmpack
+    )
