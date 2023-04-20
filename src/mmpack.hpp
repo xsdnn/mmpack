703531c791b51501bfbe3aed85b81f4f74dfb10d
akrozhin <akrozhin@miem.hse.ru>
2023-04-20 19:04:29 +0300

    Add vector && tensor def

--- src/mmpack.hpp
+++ src/mmpack.hpp
@@ -1,3 +1,12 @@
+#include <vector>
+
 #include "utils/macro.hpp"
 #include "config.hpp"
+
 #include "utils/allocator.hpp"
+
+namespace mmpack {
+    typedef std::vector<mm_scalar, aligned_allocator<mm_scalar, 64>> vector;
+    typedef std::vector<vector> tensor;
+} // mmpack
+
